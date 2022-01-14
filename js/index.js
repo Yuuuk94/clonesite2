520bb41d5cb1a99635da3b0c2342a9961a513add
Yuuuk94 <yuuuk94@gmail.com>
2022-01-14 17:22:22 +0900

    20220114

--- js/index.js
+++ js/index.js
@@ -19,5 +19,40 @@ $('body')
 $('header').load('incom.html header >div',loadAft);
 $('footer').load('incom.html footer >div');
 
-
+let Bn1 = '',Bn2 = '', num=0, lengthBn1, interval;
+$.ajax({
+    url:'./js/data.json',
+    success:function(data){
+        lengthBn1=data.mainBn1.length;
+        $.each(data.mainBn1,function(k, v){
+            Bn1 += `<a href="#"><img src="${v.src}" alt="#"/></a>`;
+        });
+        $('.s1-img')
+        .html(Bn1)
+        .css({
+            'height':`${1000}px`
+        });
+        $('.s1-nav span').eq(3).text(`0${lengthBn1}`);
+        interval = setInterval(function(){
+            $('.s1-img a').eq(num).fadeOut(1000);
+            num++;
+            $('.s1-nav span').eq(0).text(`0${num+1}`);
+            $('.s1-nav span span').css({
+                'width':`${(num+1)*33.3}%`
+            });
+            if(num==lengthBn1)
+            num=0;
+            $('.s1-img a').eq(num).fadeIn(1000);
+            $('.s1-nav span').eq(0).text(`0${num+1}`);
+            $('.s1-nav span span').css({
+                'width':`${(num+1)*33.6}%`
+            });
+        }, 5000);
+        
+        $.each(data.mainBn2,function(k, v){
+            Bn2 += `<p><img src="${v.src}" alt="#"/></p>`;
+        })
+        $('.s2-img').html(Bn2);
+    }
+});
 
